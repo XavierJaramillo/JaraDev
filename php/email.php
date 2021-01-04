fb52a7fdfdfc0e7fb9b78e7f2c9b985ecec9f756
Xavi Jaramillo Vives <xavijvives@gmail.com>
2021-01-04 20:41:24 +0100

    Implementacion modal y envio email en proceso

---
+++ php/email.php
@@ -0,0 +1,18 @@
+<?php
+
+//Recogemos variables
+$nombre = $_REQUEST['nombre'];
+$email = $_REQUEST['email'];
+$mensaje = $_REQUEST['mensaje'];
+
+//Validamos que no hay variables con valor nulo
+if(empty($nombre) || empty($email) || empty($mensaje)) {
+    echo "<script>alert('Hay algun valor nulo!')</script>";
+} else {
+    mail('xavijvives@gmail.com', 'Webtech Message', $mensaje, 'Form: $nombre <$email>');
+    echo "<script>alert('Correo enviado correctamente!');
+    window.history.log(-1);
+    </script>";
+    
+}
+?>
