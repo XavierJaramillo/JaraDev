35c2204ef395ffb3fc422b2155bbcd416943c300
Xavi Jaramillo Vives <xavijvives@gmail.com>
2021-01-07 19:57:36 +0100

    nav links

--- js/code.js
+++ js/code.js
@@ -1,3 +1,66 @@
+// modal control
 $('#myModal').on('shown.bs.modal', function() {
     $('#myInput').trigger('focus')
 })
+
+// panel control
+window.onload = function() {
+    setPanel('init');
+};
+
+function setPanel(panel) {
+    panelInicio = document.getElementById('inicio');
+    panelSobremi = document.getElementById('sobremi');
+    panelExperiencia = document.getElementById('experiencia');
+    panelProyectos = document.getElementById('proyectos');
+
+    switch (panel) {
+        case 'init':
+            initPanel(panelInicio);
+            break;
+        case 'inicio':
+            setInicio(panelInicio, panelSobremi, panelExperiencia, panelProyectos);
+            break;
+        case 'sobremi':
+            setSobremi(panelInicio, panelSobremi, panelExperiencia, panelProyectos);
+            break;
+        case 'experiencia':
+            setExperiencia(panelInicio, panelSobremi, panelExperiencia, panelProyectos);
+            break;
+        case 'proyectos':
+            setProyectos(panelInicio, panelSobremi, panelExperiencia, panelProyectos);
+            break;
+    }
+}
+
+function initPanel(panelInicio) {
+    panelInicio.style.display = "flex";
+}
+
+function setInicio(panelInicio, panelSobremi, panelExperiencia, panelProyectos) {
+    panelInicio.style.display = "flex";
+    panelSobremi.style.display = "none";
+    panelExperiencia.style.display = "none";
+    panelProyectos.style.display = "none";
+}
+
+function setSobremi(panelInicio, panelSobremi, panelExperiencia, panelProyectos) {
+    panelInicio.style.display = "none";
+    panelSobremi.style.display = "block";
+    panelExperiencia.style.display = "none";
+    panelProyectos.style.display = "none";
+}
+
+function setExperiencia(panelInicio, panelSobremi, panelExperiencia, panelProyectos) {
+    panelInicio.style.display = "none";
+    panelSobremi.style.display = "none";
+    panelExperiencia.style.display = "block";
+    panelProyectos.style.display = "none";
+}
+
+function setProyectos(panelInicio, panelSobremi, panelExperiencia, panelProyectos) {
+    panelInicio.style.display = "none";
+    panelSobremi.style.display = "none";
+    panelExperiencia.style.display = "none";
+    panelProyectos.style.display = "block";
+}
